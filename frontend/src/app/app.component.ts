2c82588042c54937b473bbe9bb0034f5cde834a7
kjubik <wojt.kubicki@gmail.com>
2025-01-10 02:04:10 +0100

    temp: add search and text panel components

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SearchPanelComponent} from "./components/layout/search-panel/search-panel.component";
+import {TextPanelComponent} from "./components/layout/text-panel/text-panel.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchPanelComponent, TextPanelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
